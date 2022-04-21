d16978a434ec49d97738e49383476e03c2456514
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-21 16:57:19 -0400

    end of day push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,23 +3,58 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+  openDB('contactDB', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('contacts' )) {
+        console.log('contactDB already exists');
+        return;
+      }
+      db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+      console.log('contactDB created');
+    },
+  });
 };
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+  const contactsDb = await openDB('contactDB', 1);
+  const tx = contactsDb.transaction('contacts', 'readwrite');
+  const store = tx.objectStore('contacts');
+  const request = store.add({ 
+    name: name, 
+    home_phone: home, 
+    cell_phone: cell, 
+    email: email 
+  });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  return result;
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
+  console.log('GET all from the database');
+  const contactsDb = await openDB('contactDB', 1);
+  const tx = contactsDb.transaction('contacts', 'readonly');
+  const store = tx.objectStore('contacts');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
   
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+  console.log('DELETE from the database', id);
+  const contactsDb = await openDB('contactDB', 1);
+  const tx = contactsDb.transaction('contacts', 'readwrite');
+  const store = tx.objectStore('contacts');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const {InjectManifest} = require('workbox-webpack-plugin');
+
 const path = require('path');
 
 module.exports = () => {
@@ -11,17 +16,56 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
       
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
+      // new InjectManifest({
+      //   swSrc: './src/src-sw.js',
+      //   swDest: 'service-worker.js',
+      // }), 
+      new WebpackPwaManifest({
+          name: 'My Progressive Web App',
+          fingerprints: false,
+          short_name: 'MyPWA',
+          description: 'My awesome Progressive Web App!',
+          background_color: '#ffffff',
+          crossorigin: 'use-credentials', 
+          publicPath: './',
+      })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
     }
-  };
+  }
 };
